6ffbe6e7303fa515a04d9e2b2b88741aa1d6efdb
gwitr <robowiko@gmail.com>
2023-01-17 23:18:36 +0100

    add color, various dispenser improvements, refactor code a little

--- main.js
+++ main.js
@@ -1,28 +1,82 @@
-const DISPENSER_RADIUS = 120;
+// TODO: Consider giving Dispenser and Circle a common parent class, maybe? (They all implement update, draw and destroy,
+//       and they all take the world as a parameter)
+
+// TODO: Reduce global state, maybe make the world be more than just an array?
+
+const DISPENSER_RADIUS = 160;
+const INITIAL_BALL_SPEED = 200;
+const GRAVITY = 220;
+const PEG_RADIUS = 10;
 
 var canvas = null;
 var ctx = null;
+var mouseX = 0, mouseY = 0;
 
-function findAngle(mouseX, mouseY, ballX, ballY, ballSpeed, dispenserRadius) {
-    let minSqrDist = 1000000.0;
-    let minAngle = -Math.PI/4;
-    let lineno = 0;
-    for (let angle = 0; angle <= Math.PI; angle += Math.PI/2 * 1/50) {
-        let angleMinSqrDist = 1000000.0;
-        for (let t = 0; t < 50; t += 0.5) {
-            let sqrDist = (mouseX - ballX - Math.cos(angle) * dispenserRadius - ballSpeed * t * Math.cos(angle)) ** 2 + (mouseY - ballY - Math.sin(angle) * dispenserRadius - ballSpeed * t * Math.sin(angle) - 1/2 * 0.5 * t**2) ** 2;
-            // if (Math.abs(angle - 0) < 0.01)
-                // ctx.fillText("IN 0.0 TEST " + sqrDist + " for " + t + " ball " + (ballSpeed * t * Math.cos(angle)) + ", " + (ballSpeed * t * Math.sin(angle) + 1/2 * t**2) + " mouse " + mouseX + ", " + mouseY, 0, 16 * ++lineno);
-            if (sqrDist < angleMinSqrDist)
-                angleMinSqrDist = sqrDist;
-        }
-        // ctx.fillText("test " + angle / Math.PI + " " + angleMinSqrDist, 0, 16 * ++lineno);
-        if (angleMinSqrDist < minSqrDist) {
-            minSqrDist = angleMinSqrDist;
-            minAngle = angle;
-        }
+class Dispenser {
+    constructor(world, x, y, r, initialBallSpeed) {
+        this.world = world;
+        this.x = x;
+        this.y = y;
+        this.r = r;
+        this.angle = 0;
+        this.initialBallSpeed = initialBallSpeed;
+
+        this._clickCallback = this.onClick.bind(this);
+        canvas.addEventListener("click", this._clickCallback);
     }
-    return minAngle;
+
+    onClick(e) {
+        if (e.button !== 0)
+            return;
+        e.preventDefault();
+
+        let circle = new Ball(this.world, this.x + this.r * Math.cos(this.angle), this.y + this.r * Math.sin(this.angle), 9);
+        circle.vx = this.initialBallSpeed * Math.cos(this.angle);
+        circle.vy = this.initialBallSpeed * Math.sin(this.angle);
+    }
+
+    findShootAngle(mouseX, mouseY, gravity) {
+        // TODO: Better search method than just looping through the angles
+        let minError = Infinity;
+        let minAngle = 0;
+        for (let angle of Array(980).fill(0).map((_, i) => i * Math.PI / 980)) {
+            let startX = this.x + Math.cos(angle) * this.r;
+            let startY = this.y + Math.sin(angle) * this.r;
+
+            let interceptT = (mouseX - startX) / (this.initialBallSpeed * Math.cos(angle));
+            if (interceptT < 0)
+                continue;
+
+            let yError = Math.abs(startY + this.initialBallSpeed * Math.sin(angle) * interceptT + 0.5 * gravity * interceptT**2 - mouseY);
+            if (yError < minError) {
+                minError = yError;
+                minAngle = angle;
+            }
+        }
+        return minAngle;
+    }
+
+    update() {
+        this.angle = this.findShootAngle(mouseX, mouseY, GRAVITY);
+    }
+
+    draw() {
+        ctx.save();
+            ctx.translate(400, 0);
+            ctx.rotate(this.angle - Math.PI / 2);
+            ctx.translate(0, this.r);
+            ctx.beginPath();
+                ctx.moveTo(-10, -25);
+                ctx.lineTo(10, -25);
+                ctx.lineTo(0, 0);
+                ctx.lineTo(-10, -25);
+            ctx.stroke();
+        ctx.restore();
+    }
+
+    destroy() {
+        canvas.removeEventListener("click", this._clickCallback);
+    }
 }
 
 class Circle {
@@ -97,14 +151,14 @@ class Circle {
 
     draw() {
         ctx.beginPath();
-        ctx.arc(this.x, this.y, this.r, 0, 2 * Math.PI);
+            ctx.arc(this.x, this.y, this.r, 0, 2 * Math.PI);
         ctx.stroke();
     
         ctx.beginPath();
-        ctx.moveTo(this.x, this.y);
-        ctx.arc(this.x, this.y, this.r, -Math.PI / 10, Math.PI / 10);
-        ctx.lineTo(this.x, this.y);
-        ctx.fillStyle = "black";
+            ctx.moveTo(this.x, this.y);
+            ctx.arc(this.x, this.y, this.r, -Math.PI / 10, Math.PI / 10);
+            ctx.lineTo(this.x, this.y);
+            ctx.fillStyle = "black";
         ctx.fill();
     }
 
@@ -113,15 +167,43 @@ class Circle {
     }
 };
 
+class Ball extends Circle {
+    constructor(world, x, y) {
+        super(world, x, y, PEG_RADIUS * 0.65, false);
+    }
+
+    static simulatePath(world, x, y, vx, vy, timeStep, seconds) {
+        // Note how we're instantiating a Circle here
+        let ball = new Circle(world.filter(x => x.isStatic), x, y, PEG_RADIUS * 0.65, false);
+        ball.vx = vx;
+        ball.vy = vy;
+        let points = [];
+        for (let t = 0; t < seconds; t += timeStep) {
+            points.push([ball.x, ball.y]);
+            ball.vy += GRAVITY * timeStep;
+            ball.update(timeStep);
+        }
+        ball.destroy();
+        return points;
+    }
+};
+
 class Peg extends Circle {
-    constructor(world, x, y, r) {
-        super(world, x, y, r, true);
+    static ORANGE = "orange";
+    static BLUE = "blue";
+    static PINK = "pink";
+    static GREEN = "green";
+
+    constructor(world, x, y, type) {
+        super(world, x, y, PEG_RADIUS, true);
+        this.type = type;
         this.timeToDie = null;
+        console.log(type);
     }
 
     collide(other) {
         let res = super.collide(other);
-        if (this.timeToDie === null && res[0] != null)
+        if (other instanceof Ball && this.timeToDie === null && res[0] != null)
             this.timeToDie = 1.5;
         return res;
     }
@@ -137,110 +219,107 @@ class Peg extends Circle {
     }
 
     draw() {
+        ctx.fillStyle = {
+            [Peg.ORANGE]: (this.timeToDie === null ? "#FFA500" : "#FFD530"),
+            [Peg.BLUE]:   (this.timeToDie === null ? "#0000FF" : "#AAAAFF")
+        }[this.type];
+
         ctx.beginPath();
-        ctx.arc(this.x, this.y, this.r, 0, 2 * Math.PI);
-        ctx.stroke();
-    
-        ctx.beginPath();
-        ctx.moveTo(this.x, this.y);
-        ctx.arc(this.x, this.y, this.r, -Math.PI / 10, Math.PI / 10);
-        ctx.lineTo(this.x, this.y);
-        ctx.fillStyle = this.timeToDie === null ? "black" : "red";
+            ctx.arc(this.x, this.y, this.r, 0, 2 * Math.PI);
         ctx.fill();
     }
-}
+};
 
 onload = () => {
     canvas = document.getElementById("game-canvas");
     ctx = canvas.getContext("2d");
+    ctx.font = "16px serif";
 
     let world = [];
+    let dispenser = new Dispenser(world, 400, 0, DISPENSER_RADIUS, INITIAL_BALL_SPEED);
 
-    for (let i = -5; i <= 5; ++i) {
-        for (let y = -2; y <= 2; ++y) {
-            let circle = new Peg(world, 400 + 70 * i + (Math.random() * 2 - 1) * 20, 420 + 70 * y + (Math.random() * 2 - 1) * 20, 12);
-            circle.vx = (Math.random() * 2 - 1) * 20;
+    // Generate level
+    for (let i = -5; i <= 5; i += 1) {
+        for (let y = -2; y <= 2; y += 1) {
+            new Peg(world,
+                400 + 70 * i + (Math.random() * 2 - 1) * 20,
+                420 + 70 * y + (Math.random() * 2 - 1) * 20,
+                Math.random() > 0.75 ? Peg.ORANGE : Peg.BLUE
+            );
         }
     }
 
-    // let circle3 = new Circle(world, 100, 200, 50);
-    // circle3.vx = 100;
-
-    let mouseX = 0, mouseY = 0;
-    let dispenserAngle = 0;
+    // Callback for updating mouse pos
     canvas.addEventListener("mousemove", e => {
         e.preventDefault();
         mouseX = e.offsetX;
         mouseY = e.offsetY;
     });
 
-    canvas.addEventListener("click", e => {
-        if (e.button !== 0)
-            return;
-        e.preventDefault();
+    // Mainloop
+    let deltaTime;
+    var mainloop = () => {
+        requestAnimationFrame(mainloop);
 
-        let circle = new Circle(world, 400 + DISPENSER_RADIUS * Math.cos(dispenserAngle), 0 + DISPENSER_RADIUS * Math.sin(dispenserAngle), 9);
-        // circle.vx = 33 /* TODO: ??? where does this come from?? */ * (mouseX - 400) / Math.sqrt(2 * (mouseY - 0) / 1);
-        // Where does that 33 come from??
-        circle.vx = 7.2 * 33 * Math.cos(dispenserAngle);
-        circle.vy = 7.2 * 33 * Math.sin(dispenserAngle);
-    });
+        // Assume 60FPS. This may result in some slight animation slowdown, but it's worth it for the more consistent physics.
+        deltaTime = 1/60;
 
-    let deltaTime = 1000/60;
-    var lastAnimFrame = null;
-    var func = timestamp => {
-        if (lastAnimFrame === null) {
-            lastAnimFrame = timestamp;
-            deltaTime = 1000/60;
-        } else {
-            deltaTime = timestamp - lastAnimFrame;
-            lastAnimFrame = timestamp;
-        }
-        if (deltaTime > 1000/5) // Probably not lag but user switching tabs and coming back, assume nothing happened
-            deltaTime = 1000/60;
-
-        // Update
-        // circle.vy += 1 * deltaTime;
+        // Update the objects in the world
         for (let obj of world) {
             if (!obj.static)
-                obj.vy += 0.5 * deltaTime;  // TODO: should dt be applied here ???
-            obj.update(deltaTime / 1000);
+                obj.vy += GRAVITY * deltaTime;
+            obj.update(deltaTime);
         }
+        dispenser.update();
 
-        // Render
+        // Render the objects in the world
         ctx.fillStyle = "white";
         ctx.rect(0, 0, canvas.width, canvas.height);
         ctx.fill();
 
-        // ctx.fillStyle = "black";
-        ctx.font = "16px serif";
-        // ctx.fillText(circle2.x + " " + circle2.y, 0, 16);
-
         for (let obj of world)
             obj.draw();
+        dispenser.draw();
 
-        dispenserAngle = findAngle(mouseX, mouseY, 400, 0, 7.2, DISPENSER_RADIUS);
-        ctx.save();
-            ctx.translate(400, 0);
-            // ctx.rotate(dispenserAngle - Math.PI / 2);
-            ctx.rotate(dispenserAngle - Math.PI / 2);
-            ctx.translate(0, DISPENSER_RADIUS);
+        // Simulate a single ball falling to generate the visible path
+        let points = Ball.simulatePath(
+            world,
+            400 + DISPENSER_RADIUS * Math.cos(dispenser.angle), 0 + DISPENSER_RADIUS * Math.sin(dispenser.angle),
+            INITIAL_BALL_SPEED * Math.cos(dispenser.angle), INITIAL_BALL_SPEED * Math.sin(dispenser.angle),
+            1/60, 50
+        );
+
+        // Draw that ball's path (TODO: This is kinda ineffecient)
+        for (let i = 0; i < 20; ++i) {
+            let pathDist = i * 25;
+
+            let lastX = points[0][0], lastY = points[0][1];
+            let totalDist = 0;
+            for (let point of points.slice(1)) {
+                let dist = Math.sqrt((point[0] - lastX) ** 2 + (point[1] - lastY) ** 2);
+                if (dist + totalDist >= pathDist) {
+                    lastX = lastX + (point[0] - lastX) / dist * (pathDist - totalDist);
+                    lastY = lastY + (point[1] - lastY) / dist * (pathDist - totalDist);
+                    break;
+                } else {
+                    totalDist += dist;
+                    lastX = point[0];
+                    lastY = point[1];
+                }
+            }
+
             ctx.beginPath();
-                ctx.moveTo(-20, 0);
-                ctx.lineTo(20, 0);
-                ctx.lineTo(0, 50);
-                ctx.lineTo(-20, 0);
+                ctx.arc(lastX, lastY, 4, 0, 2 * Math.PI);
             ctx.stroke();
-        ctx.restore();
+        }
 
+        // Draw the cursor position as a + (it looks cool!) 
         ctx.beginPath();
             ctx.moveTo(mouseX - 10, mouseY);
             ctx.lineTo(mouseX + 10, mouseY);
             ctx.moveTo(mouseX, mouseY - 10);
             ctx.lineTo(mouseX, mouseY + 10);
         ctx.stroke();
-
-        requestAnimationFrame(func);
     }
-    requestAnimationFrame(func);
+    requestAnimationFrame(mainloop);
 };
